1245e494da95e2184d744dcbc8d594b4673af98e
Djordjevic <biljanadvic@gmail.com>
2026-01-30 22:20:09 +0100

    Ajout de la fonction Collision des bords de l'ecran

--- Jeu1/js/Jeux.js
+++ Jeu1/js/Jeux.js
@@ -33,17 +33,60 @@ export default class Jeux {
         this.joueur.vx = 0;
         this.joueur.vy = 0;
 
-        if (this.inputStates.ArrowLeft)  this.joueur.vx = -3;
+        /*if (this.inputStates.ArrowLeft)  this.joueur.vx = -3;
         if (this.inputStates.ArrowRight) this.joueur.vx =  3;
         if (this.inputStates.ArrowUp)    this.joueur.vy = -3;
         if (this.inputStates.ArrowDown)  this.joueur.vy =  3;
+        */
+       if(this.inputStates.ArrowRight) {
+            this.joueur.vx = 3;
+        } 
+        if(this.inputStates.ArrowLeft) {
+            this.joueur.vx = -3;
+        } 
+
+        if(this.inputStates.ArrowUp) {
+            this.joueur.vy = -3;
+        } 
+
+        if(this.inputStates.ArrowDown) {
+            this.joueur.vy = 3;
+        } 
 
         this.joueur.move();
+        this.CollisionBordsEcran();
     }
 
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.joueur.draw(this.ctx);
     }
+
+    CollisionBordsEcran() {
+    // Gauche
+        if (this.joueur.x < 0) {
+            this.joueur.x = 0;
+            this.joueur.vx = 0;
+        }
+
+        // Droite
+        if (this.joueur.x + this.joueur.size > this.canvas.width) {
+            this.joueur.x = this.canvas.width - this.joueur.size;
+            this.joueur.vx = 0;
+        }
+
+    // Haut
+        if (this.joueur.y < 0) {
+            this.joueur.y = 0;
+            this.joueur.vy = 0;
+        }
+
+    // Bas
+        if (this.joueur.y + this.joueur.size > this.canvas.height) {
+            this.joueur.y = this.canvas.height - this.joueur.size;
+            this.joueur.vy = 0;
+        }
+    }   
+
 }
 
